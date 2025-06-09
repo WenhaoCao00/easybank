e20e9c12762009eda7eb75b9b0b8f77a3eb759d1
Wenhao Cao <hankcaownehao@gmail.com>
2025-06-09 15:13:12 +0200

    add viper ENV

--- main.go
+++ main.go
@@ -6,22 +6,26 @@ import (
 
 	"github.com/WenhaoCao00/easybank/api"
 	db "github.com/WenhaoCao00/easybank/db/sqlc"
+	"github.com/WenhaoCao00/easybank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/easy_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:7999"
-)
+
 func main(){
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
+
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
