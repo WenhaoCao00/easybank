634a405dac7fd2ca123bdc823349384983e587ee
WenhaoCao <hankcaowenhao@gmail.com>
2025-07-08 14:14:25 +0200

    updated grpc gateway

--- Makefile
+++ Makefile
@@ -43,10 +43,10 @@ mock:
 
 proto:
 	rm -f pb/*.go
-	find proto -name "*.proto" | xargs protoc \
-	    --proto_path=proto \
-	    --go_out=pb --go_opt=paths=source_relative \
-	    --go-grpc_out=pb --go-grpc_opt=paths=source_relative
+	protoc --proto_path=proto --go_out=pb --go_opt=paths=source_relative \
+	--go-grpc_out=pb --go-grpc_opt=paths=source_relative \
+	--grpc-gateway_out=pb --grpc-gateway_opt=paths=source_relative \
+	proto/*.proto
 
 evans:
 	evans --host localhost --port 8999 -r repl
--- main.go
+++ main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
 	"github.com/WenhaoCao00/easybank/api"
 	db "github.com/WenhaoCao00/easybank/db/sqlc"
 	"github.com/WenhaoCao00/easybank/gapi"
 	"github.com/WenhaoCao00/easybank/pb"
 	"github.com/WenhaoCao00/easybank/util"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 
-func main(){
+func main() {
 	config, err := util.LoadConfig(".")
 	if err != nil {
 		log.Fatal("cannot load config:", err)
@@ -28,7 +32,7 @@ func main(){
 	}
 
 	store := db.NewStore(conn)
-	// go runGatewayServer(config, store)
+	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
 }
 
@@ -37,7 +41,7 @@ func runGrpcServer(config util.Config, store db.Store) {
 	if err != nil {
 		log.Fatal("cannot create server:", err)
 	}
- 
+
 	grpcServer := grpc.NewServer()
 	pb.RegisterEasyBankServer(grpcServer, server)
 	reflection.Register(grpcServer)
@@ -54,6 +58,45 @@ func runGrpcServer(config util.Config, store db.Store) {
 	}
 }
 
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterEasyBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server:", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener:", err)
+	}
+
+	log.Printf("start HTTP gateway server at %s", listener.Addr().String())
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("cannot start HTTP gateway server:", err)
+	}
+}
 
 func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
